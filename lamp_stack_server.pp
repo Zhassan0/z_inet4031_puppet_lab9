d9c0a446b4e7e8c67faff54eb17849c127e8ac53
Zhassan0 <zohassan57@gmail.com>
2023-11-28 20:11:20 +0000

    first commit

---
+++ lamp_stack_server.pp
@@ -0,0 +1,23 @@
+package { 'apache2':
+  ensure => installed,
+}
+
+package { 'php':
+  ensure => installed,
+  notify  => Service['apache2'],
+  require => [Package['apache2']],
+}
+file { '/var/www/html/phpinfo.php':
+  source => '/home/zm/zh_inet4031_puppet_lab9/phpinfo.php',
+  notify  => Service['apache2'],
+  require => [Package['apache2']]
+
+}
+
+service { 'apache2':
+  ensure => running,
+  enable => true,
+  require => [Package['apache2'], Package['php']]
+}
+
+mariadb-server 
